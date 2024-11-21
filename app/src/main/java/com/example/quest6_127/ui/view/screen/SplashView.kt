00b2e37d30d4cf8f49d488164111fde4549e4917
Alan21103 <syahlansaputra03@gmail.com>
2024-11-22 02:04:28 +0700

    Menambahkan image dan button pada tampilan splash

--- app/src/main/java/com/example/quest6_127/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/quest6_127/ui/view/screen/SplashView.kt
@@ -1,13 +1,23 @@
 package com.example.quest6_127.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quest6_127.R
 
 @Composable
@@ -24,7 +34,22 @@ fun SplashView(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Image(
+            painter = painterResource(
+                id = R.drawable.umy3
+            ),
+            contentDescription = "",
+            modifier = Modifier.size(150.dp)
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = { onMulaiButton() }, modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .size(40.dp),
+        ) {
+            Text(text = "Mulai",
+                fontSize = 15.sp
+            )
+        }
     }
-
 }
