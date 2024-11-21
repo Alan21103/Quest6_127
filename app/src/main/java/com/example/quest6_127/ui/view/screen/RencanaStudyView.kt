5dd86f8715fe15b544319bd9bb6852fec22caa3d
Alan21103 <syahlansaputra03@gmail.com>
2024-11-22 02:26:19 +0700

    Menambahkan header pada form rencana study

--- app/src/main/java/com/example/quest6_127/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_127/ui/view/screen/RencanaStudyView.kt
@@ -1,9 +1,15 @@
 package com.example.quest6_127.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,7 +17,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.quest6_127.ui.model.Mahasiswa
 
@@ -38,6 +46,20 @@ fun RencanaStudyView(
             .padding(top = 40.dp ),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Spacer(modifier = Modifier.padding(start = 16.dp))
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
 
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy3),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(45.dp)
+            )
+        }
     }
 }
