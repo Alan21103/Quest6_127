6722ce31f1a7ef29c59b0199441b5a1ee2ca2f10
Alan21103 <syahlansaputra03@gmail.com>
2024-11-22 02:03:13 +0700

    Menambahkan color dan membuat tampilan splash

---
+++ app/src/main/java/com/example/quest6_127/ui/view/screen/SplashView.kt
@@ -0,0 +1,30 @@
+package com.example.quest6_127.ui.view.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.quest6_127.R
+
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit,
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(
+                id = R.color.primary
+            )
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+
+}
