6e3f5b6d2286499793176137cecf4fe735b4699c
Alan21103 <syahlansaputra03@gmail.com>
2024-11-22 01:56:48 +0700

    Menggantikan CompileSdk dan targetSDK menjadi 35, dan menambahkan depedency navigation dan view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.quest6_127"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.quest6_127"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    //navigation
+    implementation("androidx.navigation:navigation-compose:2.8.3")
 }
